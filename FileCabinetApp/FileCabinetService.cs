24e4e5521659c1d313d623800b53c18a0006daaa
zack <zigiska@gmail.com>
2021-04-08 20:28:23 +0300

    Add guard clauses to CreateRecord method.

--- FileCabinetApp/FileCabinetService.cs
+++ FileCabinetApp/FileCabinetService.cs
@@ -12,6 +12,7 @@ namespace FileCabinetApp
 
         public int CreateRecord(string firstName, string lastName, DateTime dateOfBirth, short digitKey, decimal account, char sex)
         {
+            this.MemberValidation(firstName, lastName, dateOfBirth, digitKey, account, sex);
             var record = new FileCabinetRecord
             {
                 Id = this.list.Count + 1,
@@ -36,6 +37,49 @@ namespace FileCabinetApp
         public int GetStat()
         {
             return this.list.Count;
+        }
+
+        private void MemberValidation(string firstName, string lastName, DateTime dateOfBirth, short digitKey, decimal account, char sex)
+        {
+            if (string.IsNullOrWhiteSpace(firstName))
+            {
+                throw new ArgumentNullException(nameof(firstName), "Firstname can't be null, emty or has only whitespaces");
+            }
+
+            if (firstName.Length < 2 || firstName.Length > 60)
+            {
+                throw new ArgumentException("Firstname can't be less 2 or bigger then 60 letters", nameof(firstName));
+            }
+
+            if (string.IsNullOrWhiteSpace(lastName))
+            {
+                throw new ArgumentNullException(nameof(lastName), "Lastname can't be null, emty or has only whitespaces");
+            }
+
+            if (lastName.Length < 2 || lastName.Length > 60)
+            {
+                throw new ArgumentException("Lastname can't be less 2 or bigger then 60 letters", nameof(lastName));
+            }
+
+            if (dateOfBirth < new DateTime(1950, 1, 1) || dateOfBirth > DateTime.Now)
+            {
+                throw new ArgumentException("Date of birth be less 2 or bigger then 60 ", nameof(dateOfBirth));
+            }
+
+            if (digitKey < 0 || digitKey > 9999)
+            {
+                throw new ArgumentException("Digit key contains 4 digits only", nameof(digitKey));
+            }
+
+            if (account < 0)
+            {
+                throw new ArgumentException("Account can't be negative", nameof(account));
+            }
+
+            if (!char.IsLetter(sex))
+            {
+                throw new ArgumentException("Sex parabeter has to contain a letter describing a sex", nameof(sex));
+            }
         }
     }
 }
